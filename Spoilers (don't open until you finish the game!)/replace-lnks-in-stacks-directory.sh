4659f4a39f0dcd08d1b936282bfcde5ff22a1ff1
wyattscarpenter <wyattscarpenter@gmail.com>
2025-10-13 00:21:34 -0700

    continue preparing for the stacks-dir fix

--- Spoilers (don't open until you finish the game!)/replace-lnks-in-stacks-directory.sh
+++ Spoilers (don't open until you finish the game!)/replace-lnks-in-stacks-directory.sh
@@ -2,9 +2,12 @@
 
 # This is a quick-and-dirty script to turn the lnk files in the directory to symlinks.
 # I haven't finished writing it yet, let alone using it.
+# As a one-off script, I won't be testing it, so it might not work anymore.
+# Run from the base Spoilers folder
 
+cd stacks-dir
 
-find . -mindepth 2 -maxdepth 2 -type f -exec foo {} \;
+find . -mindepth 2 -maxdepth 2 -type f -exec echo {} \;
 
 exit
 #don't worry about this, it's just exploratory code
